4006151f36ecf36d4c2594f5e4fbbad66334b149
Imagineee.inc <59028351+imagineeeinc@users.noreply.github.com>
2020-01-20 15:49:01 +0000

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {		System.out.println("A SwiftO AI Software & Imagineee Quantum Labs Software");
+
+		
+
+		System.out.println("Welcome to Mini Intelegent Computer");
+
+	//This is a Self-Contained Package
+
+			}
+
+}

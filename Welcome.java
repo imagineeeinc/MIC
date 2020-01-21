a72ebe999ba69f1d6c166db40f299f4e5978ba68
Imagineee <59028351+imagineeeinc@users.noreply.github.com>
2020-01-21 08:13:22 +0000

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,10 +1,8 @@
 public class Main {
 
-	public static void main(String[] args) {		System.out.println("A SwiftO AI Software & Imagineee Quantum Labs Software");
-
-		
-
-		System.out.println("Welcome to Mini Intelegent Computer");
+	public static void main(String[] args) {		
+                System.out.println("A SwiftO AI Software & Imagineee Quantum Labs Software");
+                System.out.println("Welcome to Mini Intelegent Computer");
 
 	//This is a Self-Contained Package
 
